e6e2cf1f53d568188f88463b5d16ad6c68876148
Your Name <roushan60000@gmail.com>
2026-03-31 22:06:21 +0530

    first commit

---
+++ src/Variable.java
@@ -0,0 +1,21 @@
+public class Variable{
+    static void main(){
+        int heights = 21;
+        int height_Roushan = 50;
+        int a = 1;
+
+        // NO reserved keyword
+        //   int class = 22;
+
+        // length
+        int Helloworld_roushan_kumar = 8;
+
+
+        // Case Sensitive
+        int Weight = 80;
+        int WEIGHT = 90;
+        System.out.println(Weight);
+        System.out.println(WEIGHT);
+
+    }
+}
---
+++ src/classical.java
@@ -0,0 +1,5 @@
+public class classical {
+    public static void main(){
+        System.out.println("Hello World");
+    }
+}
