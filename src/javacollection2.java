86ce6a00251cb553c26436b520f1965fe221a661
Your Name <roushan60000@gmail.com>
2026-04-28 22:32:54 +0530

    add some another java collection

---
+++ src/javacollection2.java
@@ -0,0 +1,102 @@
+import java.util.*;
+
+public class javacollection2 {
+    static void main() {
+//        Queue<Integer> q = new LinkedList<>();
+//        q.offer(10);
+//        q.offer(20);
+//        System.out.println(q);
+//        System.out.println(q.peek());
+//        System.out.println(q.poll());
+//        System.out.println(q.poll());
+//
+//
+//
+//        Queue<Integer> a = new ArrayDeque<>();
+//        a.offer(10);
+//        a.offer(20);
+//        System.out.println(a);
+//        System.out.println(a.peek());
+//
+//
+//
+//        Deque<Integer> s = new ArrayDeque<>();
+//        s.push(10);
+//        s.push(20);
+//        s.push(30);
+//        System.out.println(s);
+//        System.out.println(s.peek());
+//        s.pop();
+//        System.out.println(s.size());
+//        System.out.println(s);
+
+//        Queue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+//        pq.offer(10);
+//        pq.offer(30);
+//        pq.offer(40);
+//        pq.offer(20);
+//        pq.offer(50);
+//        System.out.println(pq);
+//        System.out.println(pq.poll());
+//        System.out.println(pq);
+//        System.out.println(pq.poll());
+//        System.out.println(pq);
+//        System.out.println(pq.poll());
+
+        // hashset
+//        Set<Integer> st = new HashSet<>();
+//        st.add(10);
+//        st.add(10);
+//        st.add(10);
+//        st.add(10);
+//        st.add(20);
+//        st.add(20);
+//        st.add(30);
+//        System.out.println(st);
+
+//
+//        Set<Integer> st1 = new HashSet<>();
+//        Set<Integer> st2 = new HashSet<>();
+//        st1.add(10);
+//        st1.add(30);
+//        st1.add(20);
+//        st1.add(40);
+//
+//        st2.add(1);
+//        st2.add(3);
+//        st2.add(2);
+//        st2.add(4);
+//
+//
+//        System.out.println(st1);
+//        System.out.println(st2);
+//
+//        System.out.println(st1);
+//        System.out.println(st2.retainAll(st1));
+
+
+//        Set<Integer> st = new LinkedHashSet<>();
+//        st.add(10);
+//        st.add(10);
+//        st.add(10);
+//        st.add(10);
+//        st.add(20);
+//        st.add(20);
+//        st.add(30);
+//        System.out.println(st);
+
+
+
+        Set<Integer> st = new TreeSet<>();
+        st.add(10);
+        st.add(10);
+        st.add(10);
+        st.add(10);
+        st.add(20);
+        st.add(20);
+        st.add(30);
+        System.out.println(st);
+
+    }
+
+}
