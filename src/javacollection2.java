17642a4934d46180955f2bef19fbae7cdaebf055
Your Name <roushan60000@gmail.com>
2026-04-29 22:22:50 +0530

    add another java collection

--- src/javacollection2.java
+++ src/javacollection2.java
@@ -1,102 +1,33 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class javacollection2 {
     static void main() {
-//        Queue<Integer> q = new LinkedList<>();
-//        q.offer(10);
-//        q.offer(20);
-//        System.out.println(q);
-//        System.out.println(q.peek());
-//        System.out.println(q.poll());
-//        System.out.println(q.poll());
-//
-//
-//
-//        Queue<Integer> a = new ArrayDeque<>();
-//        a.offer(10);
-//        a.offer(20);
-//        System.out.println(a);
-//        System.out.println(a.peek());
-//
-//
-//
-//        Deque<Integer> s = new ArrayDeque<>();
-//        s.push(10);
-//        s.push(20);
-//        s.push(30);
-//        System.out.println(s);
-//        System.out.println(s.peek());
-//        s.pop();
-//        System.out.println(s.size());
-//        System.out.println(s);
+        Map<String, String> mapping = new HashMap<>();
+        mapping.put("in","India");
+        mapping.put("en","England");
+        mapping.put("un","United State");
+        mapping.put("i","I");
+        System.out.println(mapping);
 
-//        Queue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
-//        pq.offer(10);
-//        pq.offer(30);
-//        pq.offer(40);
-//        pq.offer(20);
-//        pq.offer(50);
-//        System.out.println(pq);
-//        System.out.println(pq.poll());
-//        System.out.println(pq);
-//        System.out.println(pq.poll());
-//        System.out.println(pq);
-//        System.out.println(pq.poll());
+        Map<String, String> table = new HashMap<>();
+        table.put("br","brazil");
 
-        // hashset
-//        Set<Integer> st = new HashSet<>();
-//        st.add(10);
-//        st.add(10);
-//        st.add(10);
-//        st.add(10);
-//        st.add(20);
-//        st.add(20);
-//        st.add(30);
-//        System.out.println(st);
+        System.out.println(table);
+        table.putAll(mapping);
+        System.out.println(table);
 
-//
-//        Set<Integer> st1 = new HashSet<>();
-//        Set<Integer> st2 = new HashSet<>();
-//        st1.add(10);
-//        st1.add(30);
-//        st1.add(20);
-//        st1.add(40);
-//
-//        st2.add(1);
-//        st2.add(3);
-//        st2.add(2);
-//        st2.add(4);
-//
-//
-//        System.out.println(st1);
-//        System.out.println(st2);
-//
-//        System.out.println(st1);
-//        System.out.println(st2.retainAll(st1));
+        table.remove("en");
+        System.out.println(table);
 
 
-//        Set<Integer> st = new LinkedHashSet<>();
-//        st.add(10);
-//        st.add(10);
-//        st.add(10);
-//        st.add(10);
-//        st.add(20);
-//        st.add(20);
-//        st.add(30);
-//        System.out.println(st);
+        System.out.println(table.get("br"));
 
+        System.out.println(table.entrySet());
 
+        System.out.println(table.values());
 
-        Set<Integer> st = new TreeSet<>();
-        st.add(10);
-        st.add(10);
-        st.add(10);
-        st.add(10);
-        st.add(20);
-        st.add(20);
-        st.add(30);
-        System.out.println(st);
+        System.out.println(table.keySet());
 
     }
-
 }
