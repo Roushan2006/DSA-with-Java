44df513f6b5ac8690d49141b80f89d350aa1c0e3
Your Name <roushan60000@gmail.com>
2026-05-01 19:49:59 +0530

    Add Bitwise Operaror

---
+++ src/BitwiseOperator.java
@@ -0,0 +1,60 @@
+public class BitwiseOperator {
+    static void main() {
+//        int a = 5;
+//        int b = 3;
+//        System.out.println(a & b);
+//        System.out.println(a | b);
+//        System.out.println(a ^ b);
+//        System.out.println(~a);
+//        System.out.println(a << b);
+//        int c = -5;
+//        System.out.println(c >> b);
+//        System.out.println(c >>> b);
+
+
+        // To find even and odd
+   //     int n = 10;
+//        if (n % 2 == 0){
+//            System.out.println("Even");
+//        }
+//        else {
+//            System.out.println("Odd");
+//        }
+
+
+//        if ((n & 1) == 1){
+//            System.out.println("Odd");
+//        }
+//        else {
+//            System.out.println("Even");
+//        }
+
+//        int count = 0;
+//        while (n != 0){
+//            if((n & 1) != 0){
+//                count++;
+//            }
+//            n = n >>1;
+//        }
+//        System.out.println("Set bit count:" +  count);
+
+//        if((n & (n - 1)) == 0){
+//            System.out.println("Yes, It's power of two");
+//        }
+//        else{
+//            System.out.println("No , It's power of two");
+//        }
+
+//        int m = 5;
+//        int n = 6;
+//        System.out.println(m);
+//        System.out.println(n);
+//        m = m ^ n;
+//        n = m ^ n;
+//        m = m ^ n;
+//        System.out.println(m);
+//        System.out.println(n);
+
+
+    }
+}
